86768510519a38eba7cff967fb08e3fbab1a308c
tumit <tumit@live.com>
2024-09-28 16:54:21 +0700

    g auth.resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { ItemsModule } from './items/items.module';
 import { ConfigModule } from '@nestjs/config';
 import { DbModule } from './db/db.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ItemsModule, ConfigModule.forRoot({ isGlobal: true }), DbModule, UsersModule]
+  imports: [ItemsModule, ConfigModule.forRoot({ isGlobal: true }), DbModule, UsersModule, AuthModule]
 })
 export class AppModule {}
