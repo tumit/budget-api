bba521ac6e9021792ad89e0467082110d00662c8
tumit <tumit@live.com>
2024-09-28 11:20:54 +0700

    g db.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ItemsModule } from './items/items.module';
 import { ConfigModule } from '@nestjs/config';
+import { DbModule } from './db/db.module';
 
 @Module({
-  imports: [ItemsModule, ConfigModule.forRoot({ isGlobal: true })]
+  imports: [ItemsModule, ConfigModule.forRoot({ isGlobal: true }), DbModule]
 })
 export class AppModule {}
