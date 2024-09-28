6e90615ba9172f4849b073fc3a2e556033257ad3
tumit <tumit@live.com>
2024-09-28 11:08:41 +0700

    import ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ItemsModule } from './items/items.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ItemsModule]
+  imports: [ItemsModule, ConfigModule.forRoot({ isGlobal: true })]
 })
 export class AppModule {}
