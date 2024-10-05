cf193a50f51b500ddfc786842f2e2aa164c30093
tumit <tumit@live.com>
2024-10-06 01:54:45 +0700

    add perf-logger

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,15 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Post, Request, UseGuards, UseInterceptors } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { LoggedInDto } from './dto/logged-in.dto';
 import { RefreshJwtAuthGuard } from './guards/refresh-jwt-auth.guard';
+import { PerfLoggerInterceptor } from 'src/interceptors/perf-logger.interceptor';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  // @UseGuards(LocalAuthGuard)
-  // @Post('login')
-  // login(@Request() request: { user: LoggedInDto }) {
-  //   const access_token = this.authService.login(request.user)
-  //   return { access_token  };
-  // }
-
+  @UseInterceptors(PerfLoggerInterceptor)
   @UseGuards(LocalAuthGuard)
   @Post('login')
   login(@Request() request: { user : LoggedInDto }) {
---
+++ src/interceptors/perf-logger.interceptor.ts
@@ -0,0 +1,32 @@
+import { CallHandler, ExecutionContext, Injectable, Logger, NestInterceptor } from '@nestjs/common';
+import { catchError, Observable, tap } from 'rxjs';
+
+@Injectable()
+export class PerfLoggerInterceptor implements NestInterceptor {
+
+  private readonly logger = new Logger();
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    
+    // get req
+    const req = context.switchToHttp().getRequest();
+    const method = req.method;
+    const url = req.url;
+    const className = context.getClass().name
+
+    // create message
+    const message = `${className} ${url} ${method}`
+
+    // init start time
+    const now = Date.now()
+
+    return next.handle().pipe(
+      tap(() => this.logger.log(`${message} finish ${Date.now() - now} ms`, PerfLoggerInterceptor.name)),
+      catchError(err => {
+        this.logger.warn(`${message} ${err} finish ${Date.now() - now} ms`, PerfLoggerInterceptor.name)
+        throw err
+      })
+    );
+
+  }
+}
